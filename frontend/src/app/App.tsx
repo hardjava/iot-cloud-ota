b720f932ce304cc7a090ddbe14ef05eec8b3326a
Junwoo Park <64635737+Boksam@users.noreply.github.com>
2025-03-29 18:42:29 -0700

    나눔스퀘어라운드 폰트 설정 (#9)

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,8 +1,22 @@
 import { RouterProvider } from "react-router";
 import { Router } from "./Router";
+import { css, Global } from "@emotion/react";
+import { fontFaces } from "../shared/styles/fonts";
 
 function App() {
-  return <RouterProvider router={Router} />;
+  return (
+    <div>
+      <Global
+        styles={css`
+          ${fontFaces}
+          * {
+            font-family: "NanumSquareRound";
+          }
+        `}
+      />
+      <RouterProvider router={Router} />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/shared/styles/fonts.ts
@@ -0,0 +1,31 @@
+import { css } from "@emotion/react";
+
+export const fontFaces = css`
+  @font-face {
+    font-family: "NanumSquareRound";
+    src: url("../../assets/fonts/NanumSquareRoundL.ttf") format("truetype");
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "NanumSquareRound";
+    src: url("../../assets/fonts/NanumSquareRoundR.ttf") format("truetype");
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "NanumSquareRound";
+    src: url("../../assets/fonts/NanumSquareRoundB.ttf") format("truetype");
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "NanumSquareRound";
+    src: url("../../assets/fonts/NanumSquareRoundEB.ttf") format("truetype");
+    font-weight: 800;
+    font-style: normal;
+  }
+`;
