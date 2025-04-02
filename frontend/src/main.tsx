0ee85c8baed5f15a538aa60f2f1faf7a98a01159
Junwoo Park <64635737+Boksam@users.noreply.github.com>
2025-04-03 00:30:25 +0900

    [Frontend] MSW 세팅 (#14)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./app/App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const start = async () => {
+  const { worker } = await import("./shared/mocks/browsers");
+  worker.start();
+
+  createRoot(document.getElementById("root")!).render(<App />);
+};
+
+start();
