c1a839ccfe3adc66daa608f4fbab4dbee0187ec4
Junwoo Park <64635737+Boksam@users.noreply.github.com>
2025-04-02 22:35:51 +0900

    NanumSquareRound -> Pretendard로 폰트 변경 (#12)

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,16 +1,14 @@
 import { RouterProvider } from "react-router";
 import { Router } from "./Router";
 import { css, Global } from "@emotion/react";
-import { fontFaces } from "../shared/styles/fonts";
 
 function App() {
   return (
     <div>
       <Global
         styles={css`
-          ${fontFaces}
           * {
-            font-family: "NanumSquareRound";
+            font-family: "Pretendard";
           }
         `}
       />
