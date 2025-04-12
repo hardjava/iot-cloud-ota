72b86e9cf1cb312726813633343eeb2361cd0a18
Junwoo Park <64635737+Boksam@users.noreply.github.com>
2025-04-12 15:35:01 +0900

    [Frontend] 메인 타일 디자인 개선 (#22)

--- frontend/src/widgets/layout/ui/MainTile.tsx
+++ frontend/src/widgets/layout/ui/MainTile.tsx
@@ -9,7 +9,7 @@ export interface MainTileProps {
 export const MainTile = ({ title, rightElement, children }: MainTileProps) => {
   return (
     <div className="flex flex-col p-8 bg-white rounded-md">
-      <div className="flex items-center justify-between mb-4">
+      <div className="flex items-center justify-between mb-8">
         <h2 className="text-base text-neutral-600">{title}</h2>
         {rightElement}
       </div>
