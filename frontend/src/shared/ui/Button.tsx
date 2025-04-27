fe58090ed0fff31f43dbee3c322962c0a9329265
Junwoo Park <64635737+Boksam@users.noreply.github.com>
2025-04-27 21:38:58 +0900

    [FrontEnd] Button 컴포넌트에 `type` 추가 (#30)
    * add variant to Button component
    
    * add type to button component

--- frontend/src/shared/ui/Button.tsx
+++ frontend/src/shared/ui/Button.tsx
@@ -4,28 +4,31 @@ import { JSX } from "react";
  * Interface for Button component props
  */
 export interface ButtonProps {
-  icon?: JSX.Element;
   title: string;
+  icon?: JSX.Element;
   onClick?: () => void;
   disabled?: boolean;
+  type?: "button" | "submit" | "reset";
   variant?: "primary" | "secondary";
 }
 
 /**
  * A reusable button component with consistent styling
  * @param {ButtonProps} props - The component props
- * @param {JSX.Element} [props.icon] - Optional icon to display inside the button
  * @param {string} props.title - Text to display inside the button
+ * @param {JSX.Element} [props.icon] - Optional icon to display inside the button
  * @param {Function} [props.onClick] - Optional click handler function
  * @param {boolean} [props.disabled] - Optional flag to disable the button
+ * @param {string} [props.type] - Type of the button (default is "button")
  * @param {string} [props.variant] - Optional variant for button styling
  * @returns {JSX.Element} Rendered button component
  */
 export const Button = ({
-  icon,
   title,
+  icon,
   onClick,
   disabled,
+  type = "button",
   variant = "primary",
 }: ButtonProps): JSX.Element => {
   const disabledStyles = disabled
@@ -42,6 +45,7 @@ export const Button = ({
       className={`px-4 py-1.5 flex gap-2 items-center justify-center text-sm transition-all duration-200 rounded-md focus:ring-2 focus:ring-offset-2 ${variantStyles[variant]} ${disabledStyles}`}
       onClick={onClick}
       disabled={disabled}
+      type={type}
     >
       {icon && <span>{icon}</span>}
       {title}
