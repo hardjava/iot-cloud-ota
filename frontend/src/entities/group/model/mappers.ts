1aec44569229b62e188b2205c360ee07f9b1e8a3
Park Junwoo <boksam1017@gmail.com>
2025-05-22 15:45:02 +0900

    change name of parameter of GroupDto

--- frontend/src/entities/group/model/mappers.ts
+++ frontend/src/entities/group/model/mappers.ts
@@ -2,14 +2,14 @@ import { Group, GroupDto } from "./types";
 
 /**
  * Maps a GroupDto object to a Group object.
- * @param {GroupDto} group - The data transfer object from the API
+ * @param {GroupDto} dto - The data transfer object from the API
  * @return {Group} The transformed group domain model
  */
-export const mapGroupDto = (group: GroupDto): Group => {
+export const mapGroupDto = (dto: GroupDto): Group => {
   return {
-    id: group.id,
-    name: group.name,
-    deviceCount: group.device_count,
-    createdAt: new Date(group.created_at),
+    id: dto.id,
+    name: dto.name,
+    deviceCount: dto.device_count,
+    createdAt: new Date(dto.created_at),
   };
 };
