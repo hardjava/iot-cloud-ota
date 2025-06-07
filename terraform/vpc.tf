f0fc382a76871f5223d2893df1e01b2feb129b7a
Junwoo Park <64635737+Boksam@users.noreply.github.com>
2025-06-07 17:59:49 +0900

    [Terraform] VPC 생성 및 서브넷 구성 (#50)

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,29 +1,89 @@
-resource "aws_default_vpc" "default" {
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
-    Name = "default vpc"
+    Name        = "iot-cloud-ota-vpc"
+    Description = "VPC for IoT iot-cloud-ota"
   }
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "webserver security group"
-  description = "Enable HTTP access on port 80"
-  vpc_id      = aws_default_vpc.default.id
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.10.0/24"
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = true
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name        = "iot-cloud-ota-public-subnet-a"
+    Description = "Public subnet for iot-cloud-ota"
   }
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.12.0/24"
+  availability_zone       = "ap-northeast-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "iot-cloud-ota-public-subnet-b"
+    Description = "Public subnet for iot-cloud-ota"
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name        = "iot-cloud-ota-private-subnet-a"
+    Description = "Private subnet for iot-cloud-ota"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-2b"
+
+  tags = {
+    Name        = "iot-cloud-ota-private-subnet-b"
+    Description = "Private subnet for iot-cloud-ota"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "iot-cloud-ota-internet-gateway"
+    Description = "Internet gateway for iot-cloud-ota"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
   }
 
   tags = {
-    Name = "webserver security group"
+    Name        = "iot-cloud-ota-public-route-table"
+    Description = "Route table for public subnets in iot-cloud-ota"
   }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
 }
