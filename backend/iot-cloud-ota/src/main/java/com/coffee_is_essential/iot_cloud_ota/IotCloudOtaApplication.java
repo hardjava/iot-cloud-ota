993576bb96581595e0bed6ec1bf42a232673d629
ssong <98401648+hardjava@users.noreply.github.com>
2025-07-07 20:33:22 +0900

    [Backend] 프로젝트 세팅 (#55)

---
+++ backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/IotCloudOtaApplication.java
@@ -0,0 +1,13 @@
+package com.coffee_is_essential.iot_cloud_ota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotCloudOtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotCloudOtaApplication.class, args);
+	}
+
+}
---
+++ backend/iot-cloud-ota/src/test/java/com/coffee_is_essential/iot_cloud_ota/IotCloudOtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee_is_essential.iot_cloud_ota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotCloudOtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
