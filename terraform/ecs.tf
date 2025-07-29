b47a6ed9b96c8c01340a6808af5b450bc98f6a93
Park Junwoo <boksam1017@gmail.com>
2025-07-29 17:59:37 +0900

    add monitoring for ecs services in frontend using grafana

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -154,3 +154,23 @@ resource "aws_iam_role_policy_attachment" "backend_secrets_manager_read" {
   role       = aws_iam_role.backend.name
   policy_arn = aws_iam_policy.secrets_manager_read.arn
 }
+
+resource "aws_iam_role" "grafana" {
+  name = "grafana-app-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = { Service = "ecs-tasks.amazonaws.com" },
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "grafana_ssm" {
+  role       = aws_iam_role.grafana.name
+  policy_arn = aws_iam_policy.ecs_exec_ssm.arn
+}
