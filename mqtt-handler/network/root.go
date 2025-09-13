bf86c3701144210b98da2a087a84ddecdaf7f76c
Junwoo Park <64635737+Boksam@users.noreply.github.com>
2025-09-13 06:06:51 -0700

    [Mqtt Handler] 대량 배포 시 MQTT 연결이 끊어지는 현상 해결 (#181)
    * fix mqtt handler disconnect error
    
    * change magic number to constants

--- mqtt-handler/network/root.go
+++ mqtt-handler/network/root.go
@@ -2,6 +2,7 @@ package network
 
 import (
 	"log"
+	"mqtt-handler/mqttclient"
 	"net/http"
 )
 
@@ -12,13 +13,13 @@ type Network struct {
 }
 
 // 새로운 Network 인스턴스를 생성하고 펌웨어 배포 요청을 처리할 라우팅 경로를 등록합니다.
-func NewNetwork() *Network {
+func NewNetwork(mqttClient *mqttclient.MQTTClient) *Network {
 	net := &Network{
 		mux: http.NewServeMux(),
 	}
 
-	newFirmwareRouter(net)
-	newAdsRouter(net)
+	newFirmwareRouter(net, mqttClient)
+	newAdsRouter(net, mqttClient)
 	return net
 }
 
