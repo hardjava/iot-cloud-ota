64353b13e9f508df2c3e1566fbc3ebfa983091b2
ssong <98401648+hardjava@users.noreply.github.com>
2025-09-17 23:24:35 +0900

    [Backend] 배포 판정 로직에 ERROR 상태 추가 (#188)

--- backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/enums/DeploymentStatus.java
+++ backend/iot-cloud-ota/src/main/java/com/coffee_is_essential/iot_cloud_ota/enums/DeploymentStatus.java
@@ -1,5 +1,5 @@
 package com.coffee_is_essential.iot_cloud_ota.enums;
 
 public enum DeploymentStatus {
-    IN_PROGRESS, SUCCESS, FAILED, TIMEOUT, CANCELLED
+    IN_PROGRESS, SUCCESS, FAILED, TIMEOUT, CANCELLED, ERROR
 }
